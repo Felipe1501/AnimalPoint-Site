0f83dac1b7b802717e9070274801fa7aaa0c1089
SymetraStack <Symetrastack@gmail.com>
2022-07-15 20:58:19 -0300

    remove field search

--- assets/script/js/index.js
+++ assets/script/js/index.js
@@ -1,25 +1,5 @@
 var $simpleCarousel = document.querySelector(".js-carousel--simple");
-let inputSearch = document.querySelector('#inputSearch');
-let body = document.querySelector('body');
-let textContainer = body.children[1].innerText.toLowerCase();
-let arrayTextContainer = textContainer.split(" ");
-let fieldQtWorkSearched = document.querySelector('.col-2')
 
-inputSearch.addEventListener('input', () => {
-  var searchClient = inputSearch.value
-  let palavra = false
-  arrayTextContainer.forEach(workSearched => {
-    if (workSearched === searchClient) {
-      let qtWorksearched = workSearched.length + 1;
-      if (qtWorksearched > 1) {
-        fieldQtWorkSearched.innerHTML = qtWorksearched
-      }
-      else {
-        fieldQtWorkSearched.innerHTML = ''
-      }
-    }
-  });
-});
 
 new Glider($simpleCarousel, {
   slidesToShow: 2,
